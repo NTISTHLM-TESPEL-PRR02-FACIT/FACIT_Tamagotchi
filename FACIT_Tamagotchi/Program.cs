9dfdb043b02e8ad44f5213b30b0c231f19152f82
Mikael Bergström <krank23@gmail.com>
2021-11-22 15:59:40 +0100

    Initial commit

---
+++ FACIT_Tamagotchi/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+
+Console.WriteLine("Welcome to Tamagotchi!");
+
+Tamagotchi myTama = new Tamagotchi();
+
+Console.WriteLine("Please choose a name for your Tamagotchi!");
+myTama.name = Console.ReadLine();
+
+Console.WriteLine($"Great! {myTama.name} is a beautiful name!");
+
+while (myTama.GetAlive() == true)
+{
+  Console.Clear();
+  myTama.PrintStats();
+  Console.WriteLine("Now what do you want to do?");
+  Console.WriteLine($"1. Teach {myTama.name} a new word");
+  Console.WriteLine($"2. Talk to {myTama.name}");
+  Console.WriteLine($"3. Feed {myTama.name}");
+  Console.WriteLine($"4. Do nothing");
+
+  string doWhat = Console.ReadLine();
+  if (doWhat == "1")
+  {
+    Console.WriteLine("What word?");
+    string word = Console.ReadLine();
+    myTama.Teach(word);
+  }
+  if (doWhat == "2")
+  {
+    myTama.Hi();
+  }
+  if (doWhat == "3")
+  {
+    myTama.Feed();
+  }
+  else
+  {
+    Console.WriteLine("Doing nothing...");
+  }
+  myTama.Tick();
+  
+}
+
+Console.WriteLine($"OH NO! {myTama.name} is dead!");
+Console.WriteLine("Press ENTER to quit");
+Console.ReadLine();
